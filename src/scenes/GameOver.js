e89b4111ccdf33549c56f66ccafb87e528605a3c
charles kyalo <charleskyalo689@gmail.com>
2021-02-28 10:12:00 +0300

    change game over background

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -1,16 +1,25 @@
 import Phaser from '../lib/phaser.js';
+
 export default class GameOver extends Phaser.Scene {
 
     constructor() {
         super('game-over')
     }
 
+
+    preload() {
+        this.load.image('background', 'assets/bg_layer1.png');
+    }
+
     create() {
 
+        this.add.image(240, 320, 'background')
+            .setScrollFactor(1, 0);
         const width = this.scale.width;
         const height = this.scale.height;
         this.add.text(width * 0.5, height * 0.5, 'Game Over', {
-            fontSize: 48
+            fontSize: 48,
+            color: "#000"
         })
             .setOrigin(0.5);
         this.input.keyboard.once('keydown-SPACE', () => {
