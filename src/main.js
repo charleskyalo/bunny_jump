39cbade64a2b8aaa8d707afdfea4bcf59d7a59a4
charles kyalo <charleskyalo689@gmail.com>
2021-02-28 19:00:55 +0300

    add starting scene

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Phaser from './lib/phaser.js';
 
 /* import Game scene */
 import Game from './scenes/Game.js';
-import GameOver from './scenes/GameOver.js'
+import GameOver from './scenes/GameOver.js';
+import StartGame from './scenes/StartGame.js';
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 480,
     height: 640,
-    scene: [Game, GameOver],
+    scene: [StartGame, Game, GameOver],
     physics: {
         default: 'arcade',
         arcade: {
